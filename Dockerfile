9f95b8a36f2a3d144109841f983786523e39583f
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-30 23:40:26 +0700

    dockerfile with env variable

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,11 @@ FROM node:trixie-slim
 
 WORKDIR /app
 
-COPY . .
+COPY express/package*.json .
+RUN npm ci
 
+COPY /express/index.js index.js
+COPY /express/app.js app.js
+
+ENV PORT=3636
 CMD [ "node", "index.js" ]
--- express/index.js
+++ express/index.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
